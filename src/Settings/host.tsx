d08897a0d1ce872aea3adaf2612049bae482f557
BAD MOTHERFUCKER <lassestausgaard@gmail.com>
2021-04-16 13:10:03 +0200

    host change

--- src/Settings/host.tsx
+++ src/Settings/host.tsx
@@ -1,5 +1,5 @@
 export const Thost = 'https://touchless-socket-io-server.herokuapp.com';
-export const host = 'http://192.168.87.128:3001';
+export const host = 'http://192.168.68.109:3001';
 
 // frederik http://192.168.68.109:3001
 
